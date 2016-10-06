58674a4cfb061bde0bd98806c355f1785f4afd3e
ramsandhya <ram.sandhya@gmail.com>
2016-10-05 20:45:52 -0400

    Numbers

--- FastForward.playground/Contents.swift
+++ FastForward.playground/Contents.swift
@@ -2,6 +2,9 @@
 
 import UIKit
 
+// String
+
+// String is a class with its properties and methods.
 var str = "Hello, playground"
 
 var firstName = "Eddie"
@@ -13,7 +16,7 @@ var bookTitle = "revenge of the crab cakes"
 // capitalized is a property of the string. So, it doesn't have "()"
 bookTitle = bookTitle.capitalized
 var chatroomAnnoyingCapsGuy = "HEY THERE HYA DOING?"
-// Lowercased is a string method. So, it has "()" 
+// Lowercased is a string method. So, it has "()"
 var lowercasedChat = chatroomAnnoyingCapsGuy.lowercased()
 
 var sentence = "What the fetch?! Heck that is crazy"
@@ -21,3 +24,17 @@ if sentence.contains("fetch") || sentence.contains("Heck") {
     sentence.replacingOccurrences(of: "fetch", with: "tuna")
     sentence.replacingOccurrences(of: "Heck", with: "Playa")
 }
+
+// Numbers
+
+var age = 30
+var weight = 200
+var someNum: Double = 1214687564457608745
+var milesRan = 56.6
+var pi = 3.14
+milesRan = pi
+
+// This returns the quotient.
+var division = 13/5
+// This returns the remainder 
+var modulous = 13 % 5
