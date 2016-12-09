a822c7987db66823fd2ed54ed1d3f42e128a0e2a
ramsandhya <ram.sandhya@gmail.com>
2016-12-08 22:55:43 -0500

    fallthrough

--- FastForward.playground/Contents.swift
+++ FastForward.playground/Contents.swift
@@ -610,6 +610,19 @@ for character in puzzleInput.characters {
     }
 }
 
+// Fallthrough
+//let integerToDescribe = 5
+let integerToDescribe = 23
+var description = "The number \(integerToDescribe) is"
+switch integerToDescribe {
+case 2, 3, 5, 7, 11, 13, 17, 19:
+    description += " a prime number, and also"
+    fallthrough
+default:
+    description += " an integer."
+}
+print(description)
+
 
 
 
