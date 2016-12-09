5bc25d955f2b9e8d1270c8bae5041926b495faef
ramsandhya <ram.sandhya@gmail.com>
2016-12-09 11:56:22 -0500

    Guard statement

--- FastForward.playground/Contents.swift
+++ FastForward.playground/Contents.swift
@@ -657,7 +657,27 @@ gameLoop: while square < finalSquare {
     }
 }
 print("Game over!")
+print("---------------")
 
+// Guard statement
+func greet(person: [String: String]) {
+    guard let name = person["name"] else {
+        return
+    }
+    
+    print("Hello \(name)!")
+    
+    guard let location = person["location"] else {
+        print("I hope the weather is nice near you.")
+        return
+    }
+    
+    print("I hope the weather is nice in \(location).")
+}
 
-
-
+greet(person: ["name": "John"])
+// Prints "Hello John!"
+// Prints "I hope the weather is nice near you."
+greet(person: ["name": "Jane", "location": "Cupertino"])
+// Prints "Hello Jane!"
+// Prints "I hope the weather is nice in Cupertino."
